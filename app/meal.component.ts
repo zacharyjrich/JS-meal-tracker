96c7d45388260399dc91bb31e45eb26ddbbd9eed
Zachary Richards <zachary.j.richards@gmail.com>
2016-10-21 14:55:36 -0700

    Fix some positioning in the meal list

--- app/meal.component.ts
+++ app/meal.component.ts
@@ -4,11 +4,13 @@ import { Meal } from './meal.model';
 @Component({
   selector: 'meal-display',
   template: `
-    <div class="meal-display">
+    <div>
       <h4>Meal:</h4>
-      <p>Name: {{ meal.name }}</p>
-      <p>Calories: {{ meal.calories }}</p>
-      <p>Details: {{ meal.details }}</p>
+      <div class="meal-display">
+        <p>Name: {{ meal.name }}</p>
+        <p>Calories: {{ meal.calories }}</p>
+        <p>Details: {{ meal.details }}</p>
+      </div>
     </div>
   `
 })
