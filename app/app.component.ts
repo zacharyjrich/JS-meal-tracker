b02357d255ee3bd91bff3d68e54577dbe8a04268
Zachary Richards <zachary.j.richards@gmail.com>
2016-10-21 09:06:23 -0700

    Add template to new-meal component and connect to app component

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,10 +8,19 @@ import { Meal } from './meal.model';
   template: `
   <div class="container text-center">
     <h1>Meal Tracker</h1>
+
+  <new-meal
+  (newMealSender)="addMeal($event)"
+  ></new-meal>
+
   </div>
   `
 })
 
 export class AppComponent {
-
+  public masterMealList: Meal[] = [
+];
+  addMeal(newMealFromChild: Meal) {
+  this.masterMealList.push(newMealFromChild);
+  }
 }
--- app/new-meal.component.ts
+++ app/new-meal.component.ts
@@ -1,14 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { Meal } from './meal.model';
 
 @Component({
   selector: 'new-meal',
   template:`
-  <h1>Enter a new meal</h1>
-
+  <h3>Enter a new meal</h3>
+  <div class="meal-form">
+    <div>
+      <label>Name:</label>
+      <input #newName>
+    </div>
+    <div>
+      <label>Calories:</label>
+      <input #newCalories>
+    </div>
+    <div>
+      <label>Details:</label>
+      <input #newDetails>
+    </div>
+    <div>
+    <button class="btn btn-primary" (click)="
+      addClicked(newName.value, newCalories.value, newDetails.value);
+      newName.value='';
+      newCalories.value='';
+      newDetails.value='';
+      ">Add Meal</button>
+    </div>
+  </div>
   `
 })
 
 export class NewMealComponent {
-
+  @Output() newMealSender = new EventEmitter();
+  addClicked(name: string, calories: number, details: string) {
+  var newMealToAdd: Meal = new Meal(name, calories, details);
+  this.newMealSender.emit(newMealToAdd);
+  }
 }
